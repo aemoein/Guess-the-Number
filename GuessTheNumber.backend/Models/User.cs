fd0c9f34f1e856e46afb1037281cfecda8564c6e
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2025-07-19 22:05:27 +0300

    User Model Added

---
+++ GuessTheNumber.backend/Models/User.cs
@@ -0,0 +1,9 @@
+namespace GuessTheNumber.backend.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public int? LowestGuesses { get; set; }
+}
