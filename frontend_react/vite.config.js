20b624e4f0aee3b26523d70fad8cf05035499c09
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2025-07-20 03:00:47 +0300

    Config updates

--- frontend_react/vite.config.js
+++ frontend_react/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss({
+          // Tailwind v4 will automatically scan these paths
+          content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}']
+        })
+      ]
+    }
+  }
 })
